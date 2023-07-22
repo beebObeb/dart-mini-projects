785cf8fef8545856805ecf1224cd8652787660b0
Samuel <samyirga1004@gmail.com>
2023-07-22 23:48:52 +0300

    A hello world program

---
+++ Hello_World/hello_world.dart
@@ -0,0 +1,8 @@
+// This is a dart program to output a 'Hello World!' message onto the debug console
+// we use the built in function print ()
+// It is used for debugging
+// the main() function is mandatory
+
+void main() {
+  print('Hello World!');
+}
