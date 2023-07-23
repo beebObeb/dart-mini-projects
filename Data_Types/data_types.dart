ea242794707b292f4122970a725def4babf30d2f
Sami <80770093+beebObeb@users.noreply.github.com>
2023-07-23 04:48:43 +0000

    Data types

---
+++ Data_Types/data_types.dart
@@ -0,0 +1,30 @@
+// Dart supports five basic data types 
+// 1- int 
+// 2- double 
+// 3- num 
+// 4- String
+// 5- dynamic 
+
+// int : for basic numbers the same as int in cpp and other c type languages 
+// double : for floating point numbers 
+// num : for generic number 
+// dynamic : to make dart act as a dynamically typed language 
+// String : the same as java's String 
+
+void main (){ 
+
+  int x = 12; 
+  double y = 12.3; 
+  num z = 34; 
+  String name = 'Dart'; 
+  dynamic variable = 12.34; 
+
+// print the values 
+
+  print (x); 
+  print(y); 
+  print(z); 
+  print(name); 
+  print(variable); 
+
+}
